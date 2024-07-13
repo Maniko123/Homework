ef6b37f2dcc4844481c0b5bd42432ee53e62eca2
Maniko123 <132006234+Maniko123@users.noreply.github.com>
2024-07-13 15:01:28 +0300

    Counter

---
+++ Счетчик/Assets/Scripts/Counter.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class Counter : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _textCounter;
+
+    private float _timeDelay = 0.5f;
+    private Coroutine _increasingCounter;
+    private WaitForSeconds _expectation;
+    private int _number;
+
+    void Start()
+    {
+        _number = 0;
+        _textCounter.text = _number.ToString();
+        _expectation = new WaitForSeconds(_timeDelay);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (_increasingCounter != null)
+            {
+                StopCoroutine(_increasingCounter);
+                _increasingCounter = null;
+            }
+            else
+            {
+                _increasingCounter = StartCoroutine(CountUp());
+            }
+        }
+    }
+
+    private IEnumerator CountUp()
+    {
+        while (true)
+        {
+            _number++;
+            _textCounter.text = _number.ToString();
+            yield return _expectation;
+        }
+    }
+}
